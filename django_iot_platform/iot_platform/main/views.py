6e354ad873dec29b0eb494c628ea3685cd3e576f
Shemin Anto <13395437+sheminanto@users.noreply.github.com>
2020-09-04 20:05:10 +0530

    setting up django

--- django_iot_platform/iot_platform/main/views.py
+++ django_iot_platform/iot_platform/main/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from django.utils import timezone
 
 
 def homepage(request):
+
     return HttpResponse("hello World")
