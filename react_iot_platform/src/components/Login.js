68b6f560b4d5c37880d9c7acd8b58db174c2f422
Shreyas S Kumar <shreyassreekripa@gmail.com>
2020-10-26 23:58:30 +0530

    Fixed minor bugs

--- react_iot_platform/src/components/Login.js
+++ react_iot_platform/src/components/Login.js
@@ -2,13 +2,10 @@ import React from 'react';
 import "../css/welcome.css";
 
 class Login extends React.Component{
-    constructor(props){
-        super(props)
 
-    }
     state = {
-        username:null,
-        password:null   
+        username:'',
+        password:''   
     }
     _handleChange = (e) => {
         this.setState({
@@ -36,7 +33,7 @@ class Login extends React.Component{
                     <h4>Login</h4>
                 </div>
                 <div className="card-content">
-                    <form onSubmit={this._handleSubmit} className="needs-validation" novalidate>
+                    <form onSubmit={this._handleSubmit}>
                         <input id="username" type="text" className="form-control" placeholder="Username" value={this.state.username} onChange={this._handleChange} required></input>
                         <input id="password" type="password" className="form-control" placeholder="Password" value={this.state.password} onChange={this._handleChange} required></input>
                         <a href="#" className="link-info" id="recoverAcc">Trouble accessing your account?</a>
