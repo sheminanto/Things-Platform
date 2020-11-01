08ad3525d3830be9189744863a2a734af67bf748
Shemin Anto <13395437+sheminanto@users.noreply.github.com>
2020-11-01 19:43:14 +0530

    learning djangorestframework

--- django_iot_platform/iot_platform/main/urls.py
+++ django_iot_platform/iot_platform/main/urls.py
@@ -14,12 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register(r'heroes', views.HeroViewSet)
+
 
 app_name = "main"
 
 urlpatterns = [
-    path("", views.homepage, name="homepage"),
+    # path("", views.homepage, name="homepage"),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 
 ]
