d2d7a86a5b48010c3833576df196926d195ad697
Shemin Anto <13395437+sheminanto@users.noreply.github.com>
2020-11-02 23:42:13 +0530

    use email for login in djoser

--- django_iot_platform/iot_platform/authapp/serializers.py
+++ django_iot_platform/iot_platform/authapp/serializers.py
@@ -2,12 +2,15 @@ from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 # from django.contrib.auth.models import User
 # from .models import User
+from django.db import models
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
 class UserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
-        # email = models.Email
+        email = models.EmailField(
+            verbose_name='email', max_length=255, unique=True)
         model = User
         fields = ('email', 'username', 'password', 'first_name', 'last_name')
+        USERNAME_FIELD = 'email'
