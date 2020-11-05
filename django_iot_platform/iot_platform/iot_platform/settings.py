2e8f1cfacf47b9eb3a519950377fa1a2da25c62d
Shemin Anto <13395437+sheminanto@users.noreply.github.com>
2020-11-05 11:05:01 +0530

    update settings.py added cors

--- django_iot_platform/iot_platform/iot_platform/settings.py
+++ django_iot_platform/iot_platform/iot_platform/settings.py
@@ -55,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # added to solve CORS
+
 ]
 
 ROOT_URLCONF = 'iot_platform.urls'
@@ -155,3 +157,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+CORS_ORIGIN_ALLOW_ALL = True  # added to solve CORS
+
