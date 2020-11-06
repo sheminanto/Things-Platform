2a9abcdbd35422cc9e929abb3745fd2f0c39f197
Shemin Anto <13395437+sheminanto@users.noreply.github.com>
2020-11-07 01:51:19 +0530

    debug=False

--- django_iot_platform/iot_platform/iot_platform/settings.py
+++ django_iot_platform/iot_platform/iot_platform/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '#%eru&$3d3r%g_##g3@za^$lt5kz(li@q!@a02=k_4_*rjga=5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
