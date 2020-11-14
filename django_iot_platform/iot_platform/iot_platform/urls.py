4970f0b4df04f3a1bbf5260363da754c0798267a
Shemin Anto <13395437+sheminanto@users.noreply.github.com>
2020-11-15 04:36:25 +0530

    adding sensor model

--- django_iot_platform/iot_platform/iot_platform/urls.py
+++ django_iot_platform/iot_platform/iot_platform/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
     path('auth/', include("authapp.urls")),
-    path('auth/', include('djoser.urls.jwt')),
+    # path('auth/', include('djoser.urls.jwt')),
     path('', include("reactapp.urls")),
 ]
